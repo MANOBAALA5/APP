d88988f18966e4d24331b8a58bf0586e0e532d48
MANOBAALA5 <154234592+MANOBAALA5@users.noreply.github.com>
2023-12-18 19:10:19 +0530

    Add files via upload

---
+++ Install.sh
@@ -0,0 +1,7 @@
+sudo apt-get install python-setuptools python-pip
+sudo apt-get install tcpdump graphviz imagemagick python-gnuplot python-crypto python-pyx
+sudo pip3 install Flask
+sudo pip3 install Flask-WTF
+sudo pip3 install geoip2
+sudo pip3 install pyx
+sudo pip3 install requests
